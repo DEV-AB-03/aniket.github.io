b5e00254687e57412a911df3a74c4f855ac038cd
ANIKET BAIDYA <72697167+DEV-AB-03@users.noreply.github.com>
2021-03-17 19:53:43 +0530

    added file

---
+++ app.js
@@ -0,0 +1,12 @@
+<script>
+// When the user scrolls down 50px from the top of the document, resize the header's font size
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("header").style.fontSize = "30px";
+  } else {
+    document.getElementById("header").style.fontSize = "90px";
+  }
+}
+</script>
